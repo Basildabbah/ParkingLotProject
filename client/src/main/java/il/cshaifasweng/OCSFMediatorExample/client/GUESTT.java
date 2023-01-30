78760040d8a3809eba15c1a9707fc16d49aa9ece
muhammad_awawdi <112385389+muhammadser@users.noreply.github.com>
2023-01-30 16:39:59 +0200

    a..

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/GUESTT.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/GUESTT.java
@@ -15,7 +15,20 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class GUESTT {
+    @FXML
+    private TextField NumberOfOrder_Check;
 
+    @FXML
+    private TextField Password_Check;
+    @FXML
+    private TextField ID_Check;
+
+    @FXML
+    private TextField ID_Cancel;
+    @FXML
+    private TextField Password_Cancel;
+    @FXML
+    private TextField NumberOfOrder_Cancel;
     @FXML
     private TextField CarNumber;
 
@@ -92,18 +105,48 @@ public class GUESTT {
     @FXML
     private CheckBox OnSite;
     @FXML
+    private Label enterlabel;
+
+    @FXML
     void FAQ(ActionEvent event) throws IOException {
         App.setRoot("faq0");
     }
 
     @FXML
-    void GuestOrder(ActionEvent event) {
+    void CheckOrderStatus(ActionEvent event) {
+        Message m = new Message("#CheckOrderStatus", ID_Check.getText(), Password_Check.getText(), NumberOfOrder_Check.getText());
+        try {
+            SimpleClient.getClient().sendToServer(m);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    void onsitecheck(ActionEvent event) {
+        if (OnSite.isSelected() == true) {
+            EnterDay.setVisible(false);
+            EnterHour.setVisible(false);
+            EnterMonth.setVisible(false);
+            EnterYear.setVisible(false);
+            enterlabel.setVisible(false);
 
-        if (preorder.isSelected()==true) {
+        } else {
+            EnterDay.setVisible(true);
+            EnterHour.setVisible(true);
+            EnterMonth.setVisible(true);
+            EnterYear.setVisible(true);
+            enterlabel.setVisible(true);
+        }
+    }
+
+    @FXML
+    void GuestOrder(ActionEvent event) throws IOException {
+
+        if (preorder.isSelected() == true) {
 
             boolean OnSite = false;
 
-            Message m = new Message("#GuestPreOrder", EnterHour.getText(), EnterDay.getText(), EnterMonth.getText(), EnterYear.getText(), ExitHour.getText(), ExitDay.getText(), ExitMonth.getText(), ExitYear.getText(), ParkingLotId.getText(), ID.getText(), Password.getText(),OnSite,CarNumber.getText(),Email.getText());
+            Message m = new Message("#GuestPreOrder", EnterHour.getText(), EnterDay.getText(), EnterMonth.getText(), EnterYear.getText(), ExitHour.getText(), ExitDay.getText(), ExitMonth.getText(), ExitYear.getText(), ParkingLotId.getText(), ID.getText(), Password.getText(), OnSite, CarNumber.getText(), Email.getText());
 
             try {
                 SimpleClient.getClient().sendToServer(m);
@@ -112,7 +155,8 @@ public class GUESTT {
             }
         }
 
-        if (OnSite.isSelected()==true) {
+        if (OnSite.isSelected() == true) {
+
             LocalTime Date1 = LocalTime.now();
             int Hour = Date1.getHour();
 
@@ -127,7 +171,7 @@ public class GUESTT {
 
             boolean OnSite = true;
 
-            Message m = new Message("#GuestOnSiteOrder", Hour, Day, Month, Year, ExitHour.getText(), ExitDay.getText(), ExitMonth.getText(), ExitYear.getText(), ParkingLotId.getText(), ID.getText(), Password.getText(),OnSite,CarNumber.getText(),Email.getText());
+            Message m = new Message("#GuestOnSiteOrder", Hour, Day, Month, Year, ExitHour.getText(), ExitDay.getText(), ExitMonth.getText(), ExitYear.getText(), ParkingLotId.getText(), ID.getText(), Password.getText(), OnSite, CarNumber.getText(), Email.getText());
 
             try {
                 SimpleClient.getClient().sendToServer(m);
@@ -136,6 +180,7 @@ public class GUESTT {
             }
         }
     }
+
 
     @FXML
     void Pricesfun(ActionEvent event) throws IOException {
@@ -154,10 +199,6 @@ public class GUESTT {
     @FXML
     void loginadminfun(ActionEvent event) throws IOException {
         App.setRoot("loginadmin");
-    }
-    @FXML
-    void loginfun(ActionEvent event) {
-
     }
     @FXML
     void initialize() {
@@ -215,6 +256,94 @@ public class GUESTT {
                                             , Response1, Response2, Response3)
                             );
                             alert.show();
+                        }
+                    }
+                }
+        );
+    }
+
+
+    @FXML
+    void CancelOrder(ActionEvent event)throws IOException  {
+
+        Message m = new Message("#CancelOrder", ID_Cancel.getText(), Password_Cancel.getText(), NumberOfOrder_Cancel.getText());
+        try {
+            SimpleClient.getClient().sendToServer(m);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Subscribe
+    public void setLabelshow1(CheckOrderStatusEvent Response) throws IOException {
+        Platform.runLater(() ->
+                {
+                    if (Response.getWarning().getMessage().equals("CheckOrderStatus")) {
+                        String Response1 = Response.getWarning().getObject1().toString();
+                        if (Response1.equals("One Of The Details is Not Right")) {
+                            Alert alert = new Alert(Alert.AlertType.WARNING,
+                                    String.format("%s", Response1)
+                            );
+                            alert.show();
+                        }
+                        if (Response1.equals("Check Order Status Accepted")) {
+                            String Response2 = Response.getWarning().getObject2().toString();
+                            String Response3 = Response.getWarning().getObject3().toString();
+                            String Response4 = Response.getWarning().getObject4().toString();
+                            String Response5 = Response.getWarning().getObject5().toString();
+                            String Response6 = Response.getWarning().getObject6().toString();
+                            String Response7 = Response.getWarning().getObject7().toString();
+                            String Response8 = Response.getWarning().getObject8().toString();
+                            String Response9 = Response.getWarning().getObject9().toString();
+                            String Response10 = Response.getWarning().getObject10().toString();
+                            String Response11 = Response.getWarning().getObject11().toString();
+                            String Response12 = Response.getWarning().getObject12().toString();
+                            String Response13 = Response.getWarning().getObject13().toString();
+                            String Response14 = Response.getWarning().getObject14().toString();
+
+
+                            Alert alert = new Alert(Alert.AlertType.WARNING, String.format(
+                                    "%s \n %s %s \n OrderID is: %s \n PersonID is: %s \n ParkingLotID is: %s \n Enter Hour is: %s \n Enter Day is: %s \n Enter Month is: %s \n Enter Year is %s \n Exit Hour is: %s \n Exit Day is: %s \n Exit Month is: %s \n Exit Year is %s \n",
+                                    Response1, Response2, Response3, Response4, Response5, Response6, Response7,
+                                    Response8, Response9, Response10, Response11, Response12, Response13, Response14)
+                            );
+                            alert.show();
+                        }
+                    }
+                }
+
+        );
+    }
+
+    @Subscribe
+    public void setLabelshow2(CancelOrderEvent Response) throws IOException {
+        Platform.runLater(() ->
+                {
+                    System.out.println("bbb");
+                    if (Response.getWarning().getMessage().equals("CancelOrder")) {
+                        String Response1 = Response.getWarning().getObject1().toString();
+                        if (Response1.equals("One Of The Details is Not Right")) {
+                            Alert alert = new Alert(Alert.AlertType.WARNING,
+                                    String.format("One Of The Details is Not Right")
+                            );
+                            alert.show();
+                        }
+                        if (Response1.equals("Your Order Has Been Canceled")) {
+                            String Response2 = Response.getWarning().getObject2().toString();
+                            String Response3 = Response.getWarning().getObject3().toString();
+                            if (Response2.equals("The Refund in Money is: ")) {
+                                Alert alert = new Alert(Alert.AlertType.WARNING,
+                                        String.format("%s \n  %s %s"
+                                                , Response1, Response2, Response3)
+                                );
+                                alert.show();
+                            }
+                            if (Response2.equals("The Refund in Hours is: ")) {
+                                Alert alert = new Alert(Alert.AlertType.WARNING,
+                                        String.format("%s \n  %s %s"
+                                                , Response1, Response2, Response3)
+                                );
+                                alert.show();
+                            }
                         }
                     }
                 }
